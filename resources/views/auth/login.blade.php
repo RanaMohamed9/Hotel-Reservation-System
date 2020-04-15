7335e68d54ae60d3f017496c6db5bfede9f7787c
omar8tarek <omar7tarek@gmail.com>
2020-04-15 21:26:59 +0200

    login added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,34 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- Font-->
+	<link rel="stylesheet" type="text/css" href="csss/nunito-font.css">
+	<!-- Main Style Css -->
+    <link rel="stylesheet" href="csss/style.css"/>
+</head>
 
-                <div class="card-body">
+<body class="form-v6">
+	<div class="page-content">
+		<div style = "width:925px;" class="form-v6-content">
+			<div  class="form-left">
+				<img style = "margin-bottom:5px;" src="imagess/insta-3.jpg" alt="form">
+			</div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
+						<div class="col-md-10 ">
+                            <h1 style="color:black;margin-top:50px;margin-bottom:55px;padding-left:180px;"   > <b>   Login  </b></h1>
+                        </div>
+                      
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                           
+                            <div class="col-md-9">
+                                <input style="margin-bottom:20px;margin-left:110px;" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  placeholder="Email Address" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +39,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                           
+                            <div class="col-md-9  ">
+                                <input  style="margin-bottom:20px;margin-left:110px;" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  placeholder="Password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -40,28 +52,34 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-0 ">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                    <input style=" height:25px;width:15px;margin-bottom:20px;margin-left:110px;" class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+									
+                                    <label style="color:black;font-weight:lighter;padding-right:8px;margin-bottom:20px;margin-left:130px;" class="form-check-label" for="remember">
+                                        {{ __('Remember me  ') }}
                                     </label>
-                                </div>
+									
+                                @if (Route::has('password.request'))
+                                    <a style="padding-left:100px;" class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot?') }}
+                                   
+								 </a>
+
+                                @endif
+								
+                                
+								
                             </div>
                         </div>
+						</div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-5 ">
+                                <button style ="margin-bottom:20px;margin-left:190px;margin-top:10px;background-color:#fe892a;font-weight:bold ;font-size:15px; height:35px;width:130px;"type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+								
                             </div>
                         </div>
                     </form>
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::get('/contact', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
